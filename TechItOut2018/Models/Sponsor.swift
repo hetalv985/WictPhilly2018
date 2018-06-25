75140cc0e4ad10004d7dc97383f7191c6985f3b5
Hetal Vora <Hetal_Vora@cable.comcast.com>
2018-06-24 22:37:05 -0400

    added sponsors data

---
+++ TechItOut2018/Models/Sponsor.swift
@@ -0,0 +1,13 @@
+//
+//  Sponsor.swift
+//  TechItOut2018
+//
+//  Created by Vora, Hetal (Contractor) on 6/24/18.
+//  Copyright © 2018 Vora, Hetal (Contractor). All rights reserved.
+//
+
+import UIKit
+
+class Sponsor: NSObject {
+
+}
